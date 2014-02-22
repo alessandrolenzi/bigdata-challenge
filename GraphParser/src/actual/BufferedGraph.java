17e616ba96d8452e1f75169a2590a1739e5d3ca9
Alessandro <aless.lenzi@gmail.com>
2014-02-22 12:11:55 +0100

    a

--- GraphParser/src/actual/BufferedGraph.java
+++ GraphParser/src/actual/BufferedGraph.java
@@ -1,16 +1,9 @@
 package actual;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.nio.BufferUnderflowException;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
 import net.sourceforge.sizeof.SizeOf;
@@ -28,76 +21,36 @@ public class BufferedGraph<N extends Node, A extends Arc> extends Graph<N, A> {
 	protected String file_name;
 	protected boolean ended = false;
 	protected Map<Integer, N> nodes;
-	protected RandomAccessFile file; 
-	private long bufferMemory = 1024L*1024L*100L;
-	private long usedMemory = 0;
 	private int minLoadedId = Integer.MAX_VALUE;
 	private int maxLoadedId = -1;
-	private MappedByteBuffer buffer;
 	private Constructor<? extends N> NConstructor;
 	private Constructor<? extends A> AConstructor;
-	
-	public BufferedGraph(String str, long max_memory, Class<? extends N> node_class, Class<? extends A> arc_class) throws FileNotFoundException, NoSuchMethodException, SecurityException {
+	private long bufferMemory = 0;
+	private long usedMemory = 0;
+	private CustomReader reader;
+	public BufferedGraph(String str, long max_memory, Class<? extends N> node_class, Class<? extends A> arc_class) throws NoSuchMethodException, SecurityException, IOException {
 		file_name = str;
-		file = new RandomAccessFile(str, "r");
-		bufferMemory = max_memory;
-		//scanner = new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream(str))));
 		NConstructor = node_class.getConstructor(int.class);
 		AConstructor = arc_class.getConstructor(int.class, int.class, double.class);
 		nodes = new TreeMap<Integer, N>();
+		bufferMemory = max_memory;
+		reader = new CustomReader(str, max_memory, 4, new char[]{'\n', ':', '\t'});				
 	}
 	
-	private int page_size = 4096;
-	private byte[] page = new byte[4096];
-	private int lastDelimiterPosition = 0;
-	private int start = 0;
-	StringBuffer line = new StringBuffer();
-	private boolean newLine = false;
-	private boolean newLineDelimiter(){return newLine;}
-	private boolean seeking = false;
-	private String nextToken() {
-		boolean found = false;
-		if (seeking)
-			try {
-				System.out.println(file.getFilePointer());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		if (start == 0) 
-			try {
-				if (buffer.remaining() < 4096) {
-					page_size = buffer.remaining();
-					buffer.get(page, 0, page_size);
-				} else buffer.get(page);
-			} catch(BufferUnderflowException exc) {return null;}
-		
-		for (; lastDelimiterPosition < page_size; lastDelimiterPosition++) {
-			char c = (char) page[lastDelimiterPosition];
-			if (c == '\t' || c == ':'){found = true; newLine=false; break;}
-			if (c == '\n'){found = true; newLine=true; break;}
-		}
-		if (found) {
-			line.append(new String(Arrays.copyOfRange(page, start, lastDelimiterPosition)));
-			start = lastDelimiterPosition+1;
-			lastDelimiterPosition++;
-			String newline = line.toString();
-			line = new StringBuffer();
-			return newline;
-		}
-		line.append(new String(Arrays.copyOfRange(page, start, lastDelimiterPosition)));
-		start = 0; lastDelimiterPosition = 0;
-		return (buffer.hasRemaining()) ? nextToken() : null;
-	}
 	
 	public void load() throws IOException, UnvalidFileFormatException {
 		try {
-			buffer = file.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, Math.min((long) (bufferMemory*1.2), file.length()));
 			int count = 0;
 			String nodeId;
-			while((nodeId=nextToken()) != null && usedMemory < bufferMemory) {
-				parseNode(nodeId);
-				count++;
+			try {
+				while((nodeId=reader.nextToken()) != null && usedMemory < bufferMemory) {
+					parseNode(nodeId);
+					count++;
+					return;
+				}
+			} catch (BufferEndedException e) {
+				e.printStackTrace();
+				System.exit(0);
 			}
 			
 			System.out.println(count+"/"+usedMemory);
@@ -106,12 +59,28 @@ public class BufferedGraph<N extends Node, A extends Arc> extends Graph<N, A> {
 		}
 	}
 	
-	private void parseNode(String nodeId) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, UnvalidFileFormatException{
+	private void parseNode(String nodeId) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, UnvalidFileFormatException, IOException, BufferEndedException{
 		try {
-			N node = this.nodeFactory(Integer.parseInt(nodeId));
-			while(!newLineDelimiter()) {
-				int dest_id = Integer.parseInt(nextToken());
-				double weight = Double.parseDouble(nextToken());
+			N node = this.nodeFactory(Integer.parseInt("0"));
+			
+			while(reader.getLastDelimiter() != '\n') {
+				int dest_id=0; double weight=0;
+				boolean which = false; 
+				try {
+					String d = reader.nextToken();
+					dest_id = Integer.parseInt(d);
+					which = true;
+					weight = Double.parseDouble(reader.nextToken());					
+				} catch (BufferEndedException buffer_ended) {
+					reader.loadNext();
+					if(which) {//exception has been thrown while getting the arc weight
+						weight = Double.parseDouble(reader.nextToken());
+					} else {
+						String str = reader.nextToken();
+						dest_id = Integer.parseInt(str);
+						weight = Double.parseDouble(reader.nextToken());
+					}
+				} 
 				A arc = this.arcFactory(node.getId(), dest_id, weight);
 				node.putArc(arc);
 				usedMemory += SizeOf.sizeOf(arc);
@@ -125,18 +94,10 @@ public class BufferedGraph<N extends Node, A extends Arc> extends Graph<N, A> {
 		}
 	}
 
-	private A arcFactory(int id, int dest, double weight) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		return AConstructor.newInstance(id, dest, weight);
-	}
-
-	private N nodeFactory(int i) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, UnvalidFileFormatException{
-		return NConstructor.newInstance(i);
-
-	}
 
 	@Override
 	public N getNode(int node_id) throws NodeNotFound {
-		if (node_id > 0 && node_id >= minLoadedId && node_id <= maxLoadedId) {
+	/*	if (node_id > 0 && node_id >= minLoadedId && node_id <= maxLoadedId) {
 			N retrieved = nodes.get(node_id);
 			if (retrieved != null) return retrieved;
 			throw new NodeNotFound("Node with id "+node_id+" not found");
@@ -176,7 +137,7 @@ public class BufferedGraph<N extends Node, A extends Arc> extends Graph<N, A> {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		*/
 		return null;
 	}
 
@@ -234,6 +195,16 @@ public class BufferedGraph<N extends Node, A extends Arc> extends Graph<N, A> {
 	public Graph<N, A> cutArcs(Predicate<A> p) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	/** Factories */
+	
+	private A arcFactory(int id, int dest, double weight) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		return AConstructor.newInstance(id, dest, weight);
+	}
+
+	private N nodeFactory(int i) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, UnvalidFileFormatException{
+		return NConstructor.newInstance(i);
+
 	}
 
 }
