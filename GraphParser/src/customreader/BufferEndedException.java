9545ab62e19a2391d2bae60bd4d226aaaceb2c84
Alessandro <aless.lenzi@gmail.com>
2014-02-22 12:20:18 +0100

    Refactoring

--- GraphParser/src/customreader/BufferEndedException.java
+++ GraphParser/src/customreader/BufferEndedException.java
@@ -5,6 +5,7 @@ public class BufferEndedException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public BufferEndedException(String message) {
+		
 		super(message);
 	}
 
--- GraphParser/src/customreader/CustomReader.java
+++ GraphParser/src/customreader/CustomReader.java
@@ -14,6 +14,7 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 public class CustomReader implements Iterator<String>{
+	
 	private RandomAccessFile file;
 	private long bufferMemory = 0;
 	private long usedMemory = 0;
