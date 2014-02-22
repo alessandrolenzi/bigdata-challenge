def233eaca5662f8a43d91135ebb7625a6067938
Alessandro <aless.lenzi@gmail.com>
2014-02-22 12:17:15 +0100

    CustomReader refactoring

---
+++ GraphParser/src/customreader/BufferEndedException.java
@@ -0,0 +1,11 @@
+package customreader;
+
+public class BufferEndedException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public BufferEndedException(String message) {
+		super(message);
+	}
+
+}
