2cfee4fdf1258b49daa0c88d4cdf3faca21843e2
Michele Carignani <michele.carignani@gmail.com>
2014-03-10 21:26:12 +0100

    again new hadoop pipeline

--- GraphParser/aggregated_graphs/TimeAggregatedArcsMapper.java
+++ GraphParser/aggregated_graphs/TimeAggregatedArcsMapper.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Mapper;
 
 
@@ -20,70 +21,11 @@ import org.apache.hadoop.mapreduce.Mapper;
 	 *
 	 */
 	public class TimeAggregatedArcsMapper extends Mapper<LongWritable, Text, Text, Text>{
-		
-		/** Aggregation period */
-		private static long period = 60 * 60 * 1000;
-		/** Array of day to be kept, e.g. {Calendar.MONDAY}*/
-		private static Integer[] dayOfWeekFilter;
-		/** Array of day to be kept, e.g. {21, 22} */
-		private static Integer[] dayOfMonthFilter;
-		/**  */
-		private static Integer[] periodFilter;
-		
-		private int monthFilter = 0;
-
-		
-		public static Integer[] parseIntArray(String s)	{
-			if (s == null){
-				return new Integer[0];
-			}
-			List<Integer> res = new LinkedList<Integer>();
-			Integer k = 0;
-			String[] items = s.split(",", -1);
-			for(String i : items){
-				k = Integer.parseInt(i.replaceAll("\\s", ""));
-				res.add(k);
-			}
-			return res.toArray(new Integer[0]);
-		}
-		
-		/**
-		 * 
-		 * @return true if element is to be removed
-		 */
-		public static boolean filterWith(Integer[] a, int v) {
-			if(a.length == 0){
-				return false;
-			}
-			boolean f = true;
-			for(Integer i : a){
-				f = i.intValue() != v;
-			}
-			return f;
-		}
-
-		public static boolean checkMonth(int m, int c){
-			return (m != 0) && (m != c);
-		}
-		
+				
 		public void map(LongWritable key, Text line, Context context) 
 				throws IOException, InterruptedException {
 			
 			Configuration conf = context.getConfiguration();
-			
-			String p = conf.get("aggregationPeriod"),
-				   mf = conf.get("monthFilter");
-			if(p != null){
-				period = (Long.parseLong(p.replaceAll("\\s", ""))) * 10 * 1000;
-			}
-			
-			if(mf != null){
-				monthFilter = (Integer.parseInt(mf.replaceAll("\\s", "")));
-			}
-			
-			periodFilter = parseIntArray(conf.get("periodFilter"));
-			dayOfMonthFilter = parseIntArray(conf.get("dayOfMonthFilter"));
-			dayOfWeekFilter = parseIntArray(conf.get("dayOfWeekFilter"));
 			
 			// Parse the line, format is Timestamp \t SourceID \t DestID \t Strength 
 			String[] record = line.toString().split("\t");
@@ -95,37 +37,29 @@ import org.apache.hadoop.mapreduce.Mapper;
 			cal.setTimeInMillis(timestamp);
 			
 			int d = cal.get(Calendar.DAY_OF_MONTH);
-			int dw = cal.get(Calendar.DAY_OF_WEEK);
+			int h = cal.get(Calendar.HOUR_OF_DAY);
 			int m = cal.get(Calendar.MONTH);
 			int y = cal.get(Calendar.YEAR);
 			
-			DateFormat form = new SimpleDateFormat("dd-MM-yyyy,HH:mm:ss aaa");
-			String s = ((d < 10) ? "0" : "" ) + d + "-" +
-					   ((m < 10) ? "0" : "" ) + m + "-" +
-					   y + "-" +
-					   ",00:00:00 AM";
-			
-			Date date = null;
-			try {
-				date = form.parse(s);
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			Long dayStart = date.getTime();
-			
-			// find Period ID
-			int pId = (int)((timestamp - dayStart ) / period);
-			
-			// Check if we are interested in this period ID
-			if(filterWith(periodFilter, pId)filterWith(dayOfMonthFilter, d) 
-				&& filterWith(dayOfWeekFilter, dw)
-				&& checkMonth(monthFilter, m - 1)){
-				return;
+			String[] aggregators = conf.get("globalAggregators").split("\n");
+			List<Aggregator> aggList = new LinkedList<Aggregator>();
+			for(String s :aggregators){
+				aggList.add(new Aggregator(s, 0));
 			}
 			
-			String day = d + "-" + m + "-" + y;
-			context.write(new Text(day + "-" + pId + "-" + record[1].toString()), new Text(record[2] + ":" + record[3]));
+			String day = d + "-" + m + "-" + y + "-" + h;
+			
+			for(Aggregator a : aggList){
+				if(a.respects(day)){
+					Text newKey = new Text(a.getIdentifier() + "-" + 
+							record[1].toString() + "-" +
+							record[2].toString());
+					Text val = new Text(record[3]);
+					context.write(newKey,val);
+					return;
+				}
+			}
+			
 		}
 		
 	}
--- GraphParser/aggregated_graphs/TimeAggregatedGraphs.java
+++ GraphParser/aggregated_graphs/TimeAggregatedGraphs.java
@@ -11,12 +11,13 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 /** Generate aggregated sums of strengths over the nodes
  *  for each hour interval
- *  
+ * 
  * @author miche
  *
  */
 
 public class TimeAggregatedGraphs {
+<<<<<<< Updated upstream
 	
 	public static void main(String[] args) throws IllegalArgumentException, IOException, ClassNotFoundException, InterruptedException {
 		Configuration conf = new Configuration();
@@ -71,5 +72,61 @@ public class TimeAggregatedGraphs {
 		System.exit(second.waitForCompletion(true) ? 0 : 1);
 		
 	}
+=======
+   
+    public static void main(String[] args) throws IllegalArgumentException, IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+        // 1. set aggregation period as multiples of 10 minutes
+
+            // e.g. to set aggregation per hour
+            // c.set("aggregationPeriod", "6");
+           
+            // e.g. to set aggregation per day
+            // c.set("aggregationPeriod", "144");
+       
+        // 2. select only a subset of periods in a day, comma separated
+       
+            // e.g. to get only the 10th hour
+            // c.set("periodFilter", "10");
+
+        // 3. select only a subset of day in the month, comma separated
+            //conf.set("dayOfMonthFilter", "5, 15, 25, 30");
+       
+        // 4. select only a month (10 or 11)
+        //    conf.set("monthFilter", "11");
+            conf.set("globalAggregators", "H:7-13;M:11;W:2;Y:2013;N:MondayMorning");
+        // 5. select only a subset of day in the week, comma separated (sunday == 1)
+            // c.set("dayOfWeekArray", "2");
+       
+        Job first =  Job.getInstance(conf, "filter of probability graphs");
+        first.setJarByClass(TimeAggregatedGraphs.class);
+        first.setMapperClass(TimeAggregatedArcsMapper.class);
+        first.setReducerClass(ProbabilitiesReducer.class);
+        first.setOutputKeyClass(Text.class);
+        first.setOutputKeyClass(Text.class);
+        /**
+         * mISSING FORMATS: CHECK
+         */
+        FileInputFormat.setInputPaths(first, new Path(args[0]));
+        FileOutputFormat.setOutputPath(first, new Path("intermediate_filter"));
+       
+        Job second = Job.getInstance(conf, "create aggregated probability graphs");
+        second.setJarByClass(TimeAggregatedGraphs.class);
+        second.setMapperClass(AverageGraph.class);
+        second.setReducerClass(MeanReducer.class);
+        second.setOutputKeyClass(Text.class);
+        second.setOutputValueClass(Text.class);
+        FileInputFormat.setInputPaths(second,  new Path("intermediate_filter/*"));
+        FileOutputFormat.setOutputPath(second, new Path(args[1]));
+
+        first.submit();
+        if (first.waitForCompletion(true)) {
+            second.submit();
+        } else System.exit(1);
+       
+        System.exit(second.waitForCompletion(true) ? 0 : 1);
+       
+    }
+>>>>>>> Stashed changes
 
 }
